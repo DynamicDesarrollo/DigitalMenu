6a10aaca7f3f8d6169750622a4811957afaccee3
DynamicDesarrollo <89891618+DynamicDesarrollo@users.noreply.github.com>
2024-06-01 09:44:16 -0500

    Packaa

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escuchar en todas las interfaces
+    port: 3000, // Puedes ajustar el puerto según tus necesidades
+  },
+  preview: {
+    host: '0.0.0.0', // Escuchar en todas las interfaces para la vista previa de producción
+    port: 3000, // Asegúrate de que coincide con el puerto que usas en Render
+  },
 })
