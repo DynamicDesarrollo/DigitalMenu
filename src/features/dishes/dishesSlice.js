691fb23785474a62643ef4dd60c67c274ae3f9d4
DynamicDesarrollo <89891618+DynamicDesarrollo@users.noreply.github.com>
2024-06-01 17:49:24 -0500

    Menu21

--- src/features/dishes/dishesSlice.js
+++ src/features/dishes/dishesSlice.js
@@ -3,17 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     dishes: {
         1: [
-            { id: 1, name: 'Bruschetta', description: 'Tostada con tomate y albahaca', price: 5 },
+            { id: 1, name: 'Costilla al barril', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$ 20.000' },
             { id: 2, name: 'Patatas Bravas', description: 'Patatas fritas con salsa picante', price: 4 },
+            { id: 3, name: 'Solomito', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$22.000' },
+            { id: 4, name: 'Bondiola', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$22.000' },
+            { id: 5, name: 'Costilla Bbq', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$20.000' },
+            { id: 6, name: 'Lomo', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$20.000' },
+            { id: 7, name: 'Chuleta', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$18.000' },
         ],
         2: [
-            { id: 3, name: 'Paella', description: 'Arroz con mariscos', price: 12 },
+            { id: 3, name: 'Chicharrón al barril', description: 'papa vapor, papa criolla, yuca frita o papa francesa, ensalada y salsas de la cada', price: '$ 20.000' },
             { id: 4, name: 'Lasaña', description: 'Pasta con carne y queso', price: 10 },
         ],
         3: [
             { id: 5, name: 'Tiramisú', description: 'Postre italiano con café y mascarpone', price: 6 },
             { id: 6, name: 'Helado', description: 'Helado artesanal de varios sabores', price: 4 },
         ],
+
     },
 };
 
