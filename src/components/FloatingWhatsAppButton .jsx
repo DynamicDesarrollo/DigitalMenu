5c0d7c8abe0bf85696bad00630d97e9115644ce4
DynamicDesarrollo <89891618+DynamicDesarrollo@users.noreply.github.com>
2024-06-04 19:43:59 -0500

    Menues9

--- src/components/FloatingWhatsAppButton .jsx
+++ src/components/FloatingWhatsAppButton .jsx
@@ -7,7 +7,7 @@ const FloatingWhatsAppButton = () => {
     const [customerPhoneNumber, setCustomerPhoneNumber] = useState('');
     const [customerName, setCustomerName] = useState('');
 
-    const restaurantPhoneNumber = '573102102203';
+    const restaurantPhoneNumber = '1234567890';
 
     const generateWhatsAppMessage = () => {
         let message = `Hola, me gustaría ordenar:\n`;
@@ -28,37 +28,28 @@ const FloatingWhatsAppButton = () => {
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="Nombre"
-                value={customerName}
-                onChange={(e) => setCustomerName(e.target.value)}
-                className="border border-gray-300 rounded py-2 px-4 mb-4 w-full"
-            />
-            <input
-                type="text"
-                placeholder="Dirección"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="border border-gray-300 rounded py-2 px-4 mb-4 w-full"
-            />
-            <input
-                type="text"
-                placeholder="Teléfono"
-                value={customerPhoneNumber}
-                onChange={(e) => setCustomerPhoneNumber(e.target.value)}
-                className="border border-gray-300 rounded py-2 px-4 mb-4 w-full"
-            />
-            <div className="fixed bottom-4 right-4">
-                <button
-                    onClick={handleWhatsAppClick}
-                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
-                    style={{ backgroundColor: '#25D366' }}
-                >
-                    Enviar pedido por WhatsApp
-                </button>
-            </div>
+        <div className="fixed-bottom-center">
+            <form>
+                <input
+                    type="text"
+                    placeholder="Nombre"
+                    value={customerName}
+                    onChange={(e) => setCustomerName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Dirección"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Teléfono"
+                    value={customerPhoneNumber}
+                    onChange={(e) => setCustomerPhoneNumber(e.target.value)}
+                />
+                <button onClick={handleWhatsAppClick}>Enviar pedido por WhatsApp</button>
+            </form>
         </div>
     );
 };
